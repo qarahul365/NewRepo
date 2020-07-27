86dc9e9ef6d0df679598a122ac1aa83aed77cee1
Rahul <qa.rahul365@gmail.com>
2020-07-27 22:15:55 +0530

    Update

--- JavaLearning/src/PackageFileHandlingAssignments/Assignment1Update.java
+++ JavaLearning/src/PackageFileHandlingAssignments/Assignment1Update.java
@@ -2,24 +2,24 @@ package PackageFileHandlingAssignments;
 
 import java.io.BufferedReader;
 import java.io.File;
-
 import java.io.FileReader;
 import java.io.IOException;
-
 import java.util.Scanner;
 
-public class Assignment1 {
-	
-	public void ReadData(int linenumber) throws IOException 
+public class Assignment1Update {
+
+		public void ReadData(int linenumber) throws IOException 
 		{
 		File f = new File("C:\\Users\\rahul_sharma\\Desktop\\Sampletext.txt");
 		FileReader fr = new FileReader(f);
 		BufferedReader br = new BufferedReader(fr);
 		String line;
-		for (int i=0;i<linenumber;i++) 
-			br.readLine();
-		line=br.readLine();
-		System.out.println(line);
+		int rowCount=0;
+		while(br.readLine()!=null)
+			rowCount++;
+		System.out.println("Total rows :" +rowCount);
+		
+		
 		
 		}
 		
